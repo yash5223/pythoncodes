46ad0c1ca6fe56ea4701e897463a4bd25ab5682f
Yash Gaware <yashgaware291@gmail.com>
2025-02-13 23:22:10 +0530

    Add files via upload

---
+++ randomuse.py
@@ -0,0 +1,6 @@
+#importing random library
+import random
+#for loop for printing random numbers 11 times
+for i in range(0,10):
+    #generating random no in between 1 to 6
+    print(random.randint(1, 6))
