21c8c56b876ed2937784991271391c00f4919aae
Yash Gaware <yashgaware291@gmail.com>
2025-02-14 00:05:09 +0530

    Add files via upload

---
+++ listdictionary.py
@@ -0,0 +1,6 @@
+#this is a list
+fruits = ["Apple", "Banana", "Cherry"]
+print(fruits[0])  # Access first item
+#this is a Dictionary
+person = {"name": "Alice", "age": 20}
+print(person["name"])  # Access value by key
